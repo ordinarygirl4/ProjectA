e6e185432fdfd43ede08880646a2d9af8c4d53f9
ellen <ellen@Dogmom.home>
2024-01-03 21:30:59 -0500

    implemented add()

--- src/a/DemoA.java
+++ src/a/DemoA.java
@@ -3,5 +3,10 @@ package a;
 public class DemoA {
 public static void main(String[] args) {
 	System.out.println("Jack");
+	
+}
+public static void add(int a, int b) {
+	System.out.println(a+b);
+	
 }
 }
